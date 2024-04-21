a7c27cdf18e078f9cda581356025c3c02e7bd792
krokodila888 <krokodila888@yandex.ru>
2024-04-21 18:37:39 +0300

    FEAT: new words

--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -1,11 +1,10 @@
-import React, { FC } from 'react';
-import { useNavigate } from "react-router-dom";
+import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './PageNotFound.module.scss';
 import Header from '../../components/Header/Header';
 import arrow from '../../images/arrow2.png';
 
 const PageNotFound: FC = () => {
-
   const navigate = useNavigate();
 
   function goBack() {
@@ -16,28 +15,19 @@ const PageNotFound: FC = () => {
     <>
       <Header />
       <section className={styles.pageNotFound}>
-        <h3 className={styles.pageNotFound__title}>
-          404
-        </h3>
-        <p 
-          className={styles.pageNotFound__text} >
-            Тут пока ничего нет.
-        </p>
-        <div 
-          className={styles.pageNotFound__arrowContainer} 
-          onClick={goBack}>
-          <img 
-            src={arrow} 
-            alt="Стрелка назад" 
+        <h3 className={styles.pageNotFound__title}>404</h3>
+        <p className={styles.pageNotFound__text}>Тут пока ничего нет.</p>
+        <div className={styles.pageNotFound__arrowContainer} onClick={goBack}>
+          <img
+            src={arrow}
+            alt="Стрелка назад"
             className={styles.pageNotFound__arrow}
           />
-            <p className={styles.pageNotFound__text}>
-              Вернитесь на главную.
-            </p>
+          <p className={styles.pageNotFound__text}>Вернитесь на главную.</p>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
 export default PageNotFound;
--- stylelint.config.js
+++ stylelint.config.js
@@ -2,5 +2,8 @@ module.exports = {
   extends: 'stylelint-config-standard-scss',
   rules: {
     'selector-class-pattern': null,
+    'selector-type-case': null,
+    'selector-type-no-unknown': null,
+    'property-no-unknown': null,
   },
 };
